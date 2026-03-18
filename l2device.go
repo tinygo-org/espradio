1b7a9bdab3a87fbc3765da6e7f2cc240d87fad10
Patricio Whittingslow <graded.sp@gmail.com>
2026-03-18 17:59:32 -0300

    remove old interface

--- l2device.go
+++ l2device.go
@@ -4,20 +4,6 @@ import "net"
 
 const MaxFrameSize = 1518
 
-// L2Device is the interface a TCP/IP stack needs from a link-layer device.
-//
-//	espradio.L2Device  (L2: raw Ethernet frames)
-//	       ↓
-//	  NetStack          (L3/L4: ARP, DHCP, IP, TCP, UDP, DNS)
-//	       ↓
-//	  implements Netdever → Use() → net.Dial / http.Get work
-type L2Device interface {
-	SendEth(frame []byte) error
-	RecvCh() <-chan []byte
-	HardwareAddr() ([6]byte, error)
-	MTU() int
-}
-
 // EthernetDevice is WIP of how ethernet device
 // API design.
 //
