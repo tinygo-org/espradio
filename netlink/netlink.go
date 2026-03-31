511e45567b189859e94b786f315f34aaff014637
Ron Evans <ron@hybridgroup.com>
2026-03-31 19:43:16 +0200

    Connect espradio to the TinyGo netlinker and netdev interfaces (#14)
    * modules: get latest lneto and drivers
    
    Signed-off-by: deadprogram <ron@hybridgroup.com>
    
    * netlink: work in progress on connecting espradio to the TinyGo drivers netlink and netdev interfaces
    
    Signed-off-by: deadprogram <ron@hybridgroup.com>
    
    * netlink: further implementation for the espradio netlink implmentation
    
    Signed-off-by: deadprogram <ron@hybridgroup.com>
    
    * clean up, fix up some stuff
    
    * add berkeley stack calling convention
    
    * fix some issues
    
    * netlink: increase memory size for tx buffer
    
    Signed-off-by: deadprogram <ron@hybridgroup.com>
    
    * remove debugging printlns
    
    Signed-off-by: deadprogram <ron@hybridgroup.com>
    
    * modules: use latest lneto package update
    
    Signed-off-by: deadprogram <ron@hybridgroup.com>
    
    ---------
    
    Signed-off-by: deadprogram <ron@hybridgroup.com>
    Co-authored-by: Patricio Whittingslow <graded.sp@gmail.com>

---
+++ netlink/netlink.go
@@ -0,0 +1,204 @@
+package netlink
+
+import (
+	"errors"
+	"net"
+	"net/netip"
+	"sync"
+	"time"
+	"runtime"
+
+	"github.com/soypat/lneto/x/xnet"
+
+	nl "tinygo.org/x/drivers/netlink"
+	"tinygo.org/x/espradio"
+)
+
+const pollTime = 5 * time.Millisecond
+
+// Esplink implements the Netlinker interface for the ESP32-C3's WiFi interface, using the espradio package and an lneto Stack.
+type Esplink struct {
+	params   *nl.ConnectParams
+	notifyCb func(nl.Event)
+
+	netstack  *espradio.Stack
+	berkeley  xnet.StackBerkeley
+	stackloop sync.Once
+}
+
+func (n *Esplink) rstack() xnet.StackRetrying {
+	return n.netstack.LnetoStack().StackRetrying(pollTime)
+}
+
+// NetConnect device to network
+func (n *Esplink) NetConnect(params *nl.ConnectParams) error {
+	if len(params.Ssid) == 0 {
+		return nl.ErrMissingSSID
+	}
+
+	err := espradio.Enable(espradio.Config{
+		Logging: espradio.LogLevelError,
+	})
+	if err != nil {
+		println("could not enable radio:", err)
+		return err
+	}
+
+	err = espradio.Start()
+	if err != nil {
+		println("could not start radio:", err)
+		return err
+	}
+
+	err = espradio.Connect(espradio.STAConfig{
+		SSID:     params.Ssid,
+		Password: params.Passphrase,
+	})
+	if err != nil {
+		println("connect failed:", err)
+		return err
+	}
+
+	nd, err := espradio.StartNetDev()
+	if err != nil {
+		println("netdev failed:", err)
+		return err
+	}
+
+	espstack, err := espradio.NewStack(nd, espradio.StackConfig{
+		Hostname:    params.Ssid,
+		MaxUDPPorts: 2,
+		MaxTCPPorts: 1,
+	})
+	if err != nil {
+		println("stack failed:", err)
+		return err
+	}
+
+	n.netstack = espstack
+
+	if n.notifyCb != nil {
+		n.notifyCb(nl.EventNetUp)
+	}
+
+	n.stackloop.Do(func() {
+		// Start stack goroutine once.
+		gostack := n.netstack.LnetoStack().StackGo(pollTime, xnet.StackGoConfig{
+			ListenerPoolConfig: xnet.TCPPoolConfig{
+				PoolSize:           2,
+				QueueSize:          4,
+				TxBufSize:          4096,
+				RxBufSize:          1024,
+				EstablishedTimeout: 2 * time.Second,
+				ClosingTimeout:     2 * time.Second,
+			},
+		})
+		n.berkeley = *xnet.NewBerkeleyStack(gostack.Socket)
+		go handleStack(espstack)
+	})
+	err = n.doDHCP()
+	if err != nil {
+		println("debug: doDHCP failed:", err.Error())
+		return err
+	}
+	return nil
+}
+
+func (n *Esplink) doDHCP() error {
+	_, err := n.netstack.SetupWithDHCP(espradio.DHCPConfig{})
+	if err != nil {
+		println("DHCP failed:", err)
+		return err
+	}
+	return nil
+}
+
+// NetDisconnect device from network
+func (n *Esplink) NetDisconnect() {
+	// TODO: implement this.  For now, just do nothing and let the connection time out.
+}
+
+// NetNotify to register callback for network events
+func (n *Esplink) NetNotify(cb func(nl.Event)) {
+	n.notifyCb = cb
+}
+
+// GetHardwareAddr returns device MAC address
+func (n *Esplink) GetHardwareAddr() (net.HardwareAddr, error) {
+	hw := n.netstack.LnetoStack().HardwareAddress()
+	return hw[:], nil
+}
+
+// GetHostByName returns the IP address of either a hostname or IPv4
+// address in standard dot notation
+func (n *Esplink) GetHostByName(name string) (netip.Addr, error) {
+	if len(name) == 0 {
+		return netip.Addr{}, errors.New("empty name")
+	} else if name[0] >= '0' && name[0] <= '9' {
+		// Special case to try for IPv4 addresses.
+		addr, err := netip.ParseAddr(name)
+		if err == nil {
+			return addr, nil
+		}
+	}
+	rstack := n.rstack()
+	addrs, err := rstack.DoLookupIP(name, 5*time.Second, 3)
+	if err != nil {
+		println("DNS lookup failed:", err)
+		return netip.Addr{}, err
+	}
+	return addrs[0], nil
+}
+
+// Addr returns IP address assigned to the interface, either by
+// DHCP or statically
+func (n *Esplink) Addr() (netip.Addr, error) {
+	return n.netstack.LnetoStack().Addr(), nil
+}
+
+// Berkely Sockets-like interface, Go-ified.  See man page for socket(2), etc.
+func (n *Esplink) Socket(domain int, stype int, protocol int) (int, error) {
+	return n.berkeley.Socket(domain, stype, protocol)
+}
+
+func (n *Esplink) Bind(sockfd int, ip netip.AddrPort) error {
+	return n.berkeley.Bind(sockfd, ip)
+}
+
+func (n *Esplink) Connect(sockfd int, host string, ip netip.AddrPort) error {
+	return n.berkeley.Connect(sockfd, host, ip)
+}
+
+func (n *Esplink) Listen(sockfd int, backlog int) error {
+	return n.berkeley.Listen(sockfd, backlog)
+}
+
+func (n *Esplink) Accept(sockfd int) (int, netip.AddrPort, error) {
+	return n.berkeley.Accept(sockfd)
+}
+
+func (n *Esplink) Send(sockfd int, buf []byte, flags int, deadline time.Time) (int, error) {
+	return n.berkeley.Send(sockfd, buf, flags, deadline)
+}
+
+func (n *Esplink) Recv(sockfd int, buf []byte, flags int, deadline time.Time) (int, error) {
+	return n.berkeley.Recv(sockfd, buf, flags, deadline)
+}
+
+func (n *Esplink) Close(sockfd int) error {
+	return n.berkeley.Close(sockfd)
+}
+
+func (n *Esplink) SetSockOpt(sockfd int, level int, opt int, value interface{}) error {
+	return n.berkeley.SetSockOpt(sockfd, level, opt, value)
+}
+
+func handleStack(stack *espradio.Stack) {
+	for {
+		send, recv, _ := stack.RecvAndSend()
+		if send == 0 && recv == 0 {
+			time.Sleep(pollTime)
+			runtime.Gosched()
+		}
+	}
+}
