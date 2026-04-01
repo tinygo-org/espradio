98ff53585a563378bc92a788be5d6dbd35248fee
Ron Evans <ron@hybridgroup.com>
2026-04-01 07:59:52 +0200

    More examples (#15)
    * gitignore
    
    Signed-off-by: deadprogram <ron@hybridgroup.com>
    
    * examples: add http-get example that uses std http package
    
    Signed-off-by: deadprogram <ron@hybridgroup.com>
    
    * example: switch http-get to use http not https
    
    Signed-off-by: deadprogram <ron@hybridgroup.com>
    
    * examples: add mqtt example
    
    Signed-off-by: deadprogram <ron@hybridgroup.com>
    
    * modules: update to latest lneto
    
    Signed-off-by: deadprogram <ron@hybridgroup.com>
    
    * docs: some further explanations in the README
    
    Signed-off-by: deadprogram <ron@hybridgroup.com>
    
    ---------
    
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ examples/http-get/main.go
@@ -0,0 +1,78 @@
+// This example gets an URL using http.Get().  URL scheme can be http or https.
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"tinygo.org/x/drivers/netdev"
+	nl "tinygo.org/x/drivers/netlink"
+	link "tinygo.org/x/espradio/netlink"
+)
+
+var (
+	ssid string
+	password string
+)
+
+func main() {
+	// wait a bit for serial
+	time.Sleep(2 * time.Second)
+
+	link := link.Esplink{}
+	netdev.UseNetdev(&link)
+
+	println("Connecting to WiFi...")
+	err := link.NetConnect(&nl.ConnectParams{
+		Ssid:       ssid,
+		Passphrase: password,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	println("Connected. Getting URL...")
+	name := "John Doe"
+	occupation := "gardener"
+
+	params := "name=" + url.QueryEscape(name) + "&" +
+		"occupation=" + url.QueryEscape(occupation)
+
+	path := fmt.Sprintf("http://httpbin.org/get?%s", params)
+
+	cnt := 0
+	for {
+		fmt.Printf("Getting %s\r\n\r\n", path)
+		resp, err := http.Get(path)
+		if err != nil {
+			fmt.Printf("problem is %s\r\n", err.Error())
+			time.Sleep(10 * time.Second)
+			continue
+		}
+
+		fmt.Printf("%s %s\r\n", resp.Proto, resp.Status)
+		for k, v := range resp.Header {
+			fmt.Printf("%s: %s\r\n", k, strings.Join(v, " "))
+		}
+		fmt.Printf("\r\n")
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Printf("%s\r\n", err.Error())
+			time.Sleep(10 * time.Second)
+			continue
+		}
+		println(string(body))
+		resp.Body.Close()
+
+		cnt++
+		fmt.Printf("-------- %d --------\r\n", cnt)
+		time.Sleep(10 * time.Second)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module tinygo.org/x/espradio
 go 1.24.4
 
 require (
-	github.com/soypat/lneto v0.0.0-20260331170330-22f29fdfb1f8
+	github.com/soypat/lneto v0.0.0-20260331220408-22c7e6c9d80b
 	tinygo.org/x/drivers v0.34.0
 )
+
+require github.com/soypat/natiu-mqtt v0.6.0
